2aa6ce86631d0be6c45af05dd09b10961f23b4d6
marianita13 <marianitacero.1913@gmail.com>
2023-10-20 13:52:09 -0500

    feat: :sparkles: Last Controller

---
+++ API/Controllers/VentaController.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Controllers;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class VentaController : BaseController
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+        private readonly IMapper _mapper;
+
+        public VentaController(IUnitOfWork unitOfWork, IMapper mapper){
+            _UnitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<IEnumerable<Venta>>> Get(){
+            var items = await _UnitOfWork.Ventas.GetAllAsync();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<IEnumerable<Venta>>> Get(int id){
+            var item = await _UnitOfWork.Ventas.GetIdAsync(id);
+            return Ok(item); 
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<Venta>> Post(VentaDto itemDto){
+            var item = _mapper.Map<Venta>(itemDto);
+            this._UnitOfWork.Ventas.Add(item);
+            await _UnitOfWork.SaveAsync();
+            if (item==null){
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(Post), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<Venta>> Put(int id, [FromBody] Venta item){
+            if (item.Id == 0){
+                item.Id=id;
+            }
+            if (item.Id != id){
+                return BadRequest();
+            }
+            if (item == null){
+                return NotFound();
+            }
+            _UnitOfWork.Ventas.Update(item);
+            await _UnitOfWork.SaveAsync();
+            return item;
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id){
+            var item = await _UnitOfWork.Ventas.GetIdAsync(id);
+            if (item == null){
+                return NotFound();
+            }
+            _UnitOfWork.Ventas.Remove(item);
+            await _UnitOfWork.SaveAsync();
+            return NoContent();
+        }
+    }
+}
